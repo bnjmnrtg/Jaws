bc836a4c2b525c3a858a15274949db31052e313a
bnjmnrtg <bnjmn_rtg@yahoo.com>
2016-07-25 19:47:03 -0500

    second commit

--- app/config/connection.js
+++ app/config/connection.js
@@ -15,14 +15,18 @@ var source = {
         database: "starwars"
     },
     jawsDB: {
-
+        port: 3306,
+        host:'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        username: 'dqyeyr99l2ae73ph',
+        password:'ri8nu2059lu5xi7f',
+        database:'zsvrwrrqky6tdf40'
         // TODO: Add your JawsDB connection info here
     }
 }
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection( //enter connection here);
+var connection = mysql.createConnection(source.jawsDB); //enter connection here);
 
 
 
